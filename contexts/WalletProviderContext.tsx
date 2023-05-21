65c28c198b456f105ecc4211dd0de76df32038d4
RyanHaraki <ryan.haraki@hotmail.com>
2023-05-21 06:35:50 -0400

    etestset

--- contexts/WalletProviderContext.tsx
+++ contexts/WalletProviderContext.tsx
@@ -1,5 +1,4 @@
-import { NextPage } from "next";
-import { FC, ReactNode } from "react";
+import { ReactNode } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
--- pages/sign-up/[[...index]].tsx
+++ pages/sign-up/[[...index]].tsx
@@ -1,6 +1,15 @@
 import { SignUp } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function Page() {
+  const user = useUser();
+
+  useEffect(() => {
+    if (user) {
+      window.location.href = "/dashboard";
+    }
+  }, [user]);
   return (
     <div className="h-screen w-full flex items-center-justify-center">
       <SignUp
